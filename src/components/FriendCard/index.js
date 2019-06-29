21ca7e70f7d55d0de9261bcd640c27edf3d267c7
JDC-RVA <jeremy@firstmotionlc.com>
2019-06-28 23:39:37 -0400

    created cards and players json data

---
+++ src/components/FriendCard/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./style.css";
+
+function FriendCard(props) {
+  return (
+    <div className="card">
+      <div className="img-container">
+        <img alt={props.name} src={props.image} />
+      </div>
+      <div className="content">
+        <ul>
+          <li>
+            <strong>Name:</strong> {props.name}
+          </li>
+          <li>
+            <strong>Occupation:</strong> {props.occupation}
+          </li>
+          <li>
+            <strong>Team:</strong> {props.team}
+          </li>
+        </ul>
+      </div>
+      <span onClick={() => props.removeFriend(props.id)} className="remove">
+        𝘅
+      </span>
+    </div>
+  );
+}
+
+export default FriendCard;
